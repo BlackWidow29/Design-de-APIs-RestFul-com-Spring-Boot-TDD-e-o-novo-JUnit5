75146f0ae1bbb00fca5b4964ae2b439dfd4ac3f8
Isabelly Dias <isabellydias29@gmail.com>
2022-03-03 17:35:37 -0300

    Iniciando com Spring boot

---
+++ primeiro-projeto-rest/src/main/java/com/escorpion/primeiroprojetorest/MeuResource.java
@@ -0,0 +1,41 @@
+package com.escorpion.primeiroprojetorest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.net.server.Client;
+
+@RestController
+public class MeuResource {
+    
+    //@RequestMapping(value = "/api/clientes/{id}", method = RequestMethod.GET)
+    @GetMapping("/api/clientes/{id}")
+    public Cliente obter(@PathVariable Long id){
+        return new Cliente("Fulaninho", "000.0000.000-00");
+    }
+
+    @PostMapping("/api/clientes")
+    public Cliente salvar(@RequestBody Cliente cliente){
+        return cliente;
+    }
+
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Long id){
+
+    }
+
+    @PutMapping("/api/clientes/{id}")
+    public Cliente atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
+        return cliente;
+    }
+}
---
+++ primeiro-projeto-rest/src/test/java/com/escorpion/primeiroprojetorest/PrimeiroProjetoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escorpion.primeiroprojetorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
